3ce0bf13e2bb60e6f49a38b9441ee664d4c5d0b5
rayen-naffouti <naffoutirayen11@gmail.com>
2022-02-25 18:15:05 +0100

    first commit

---
+++ backend/app/Http/Controllers/PersonnelController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Personnel;
+
+class PersonnelController extends Controller
+{
+    public function index()
+    {
+        
+         $personnels=Personnel::
+            join('qualifications','personnels.PERS_CODE_QUALIF','qualifications.QUALIF_CODE')
+            ->join('natureagents','personnels.PERS_NATURAGENT_93','natureagents.NATAGENT_CODE_93')
+             ->get();
+   
+         return response()->json($personnels,200);
+    }
+    public function abse()
+    {
+      $personnels=Personnel::
+            join('qualifications','personnels.PERS_CODE_QUALIF','qualifications.QUALIF_CODE')
+            ->join('natureagents','personnels.PERS_NATURAGENT_93','natureagents.NATAGENT_CODE_93')
+             ->get();
+      $count = array();
+           for($i = 1;$i<=count($personnels);$i++)
+          {
+              $count[]=Personnel::
+                   join('absences','personnels.PERS_MAT_95','absences.ABS_NUMORD_93')
+                  ->where('personnels.PERS_MAT_95',$i)
+                   ->get() 
+                   ->count() ;    
+           }
+         return response()->json($count,200);
+    }
+
+    public function show($PERS_MAT_95)
+    {
+        // $personnels = Absence::where('ABS_NUMORD_93',$PERS_MAT_95)->get();
+        // dd($personnels);
+        // return view('Personnel.show')->with('personnels', $personnels);
+        $personnels=Personnel::
+            join('absences','personnels.PERS_MAT_95','ABS_NUMORD_93')
+            ->join('natabses','absences.ABS_NAT_9','natabses.CODE_ABS')
+            ->where('personnels.PERS_MAT_95',$PERS_MAT_95)
+            ->get() ;
+                // $count =  $personnels->count();
+                // dd($count);
+            return  $personnels;
+    }
+}
---
+++ backend/routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\PersonnelController;   
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get('personnels',[PersonnelController::class,'index']);
+Route::get('personnels/abse',[PersonnelController::class,'abse']);
+Route::get('personnels/{PERS_MAT_95}',[PersonnelController::class,'show']);
+
