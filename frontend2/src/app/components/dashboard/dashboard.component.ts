3870730135e69a80fca7f405001411a508907c68
rayen-naffouti <naffoutirayen11@gmail.com>
2022-06-08 23:09:10 +0200

    add rapport

--- frontend2/src/app/components/dashboard/dashboard.component.ts
+++ frontend2/src/app/components/dashboard/dashboard.component.ts
@@ -62,25 +62,19 @@ export class DashboardComponent implements OnInit {
     var chart2 = new Chart("chart2", {
       type: 'bar',
       data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          labels: ['Red', 'Blue',  'Purple'],
           datasets: [{
               label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
+              data: [12, 19,  2],
               backgroundColor: [
                   'rgba(255, 99, 132, 0.2)',
                   'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
+                  'rgba(153, 102, 255, 0.2)'
               ],
               borderColor: [
                   'rgba(255, 99, 132, 1)',
                   'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
+                  'rgba(153, 102, 255, 1)'
               ],
               borderWidth: 1
           }]
